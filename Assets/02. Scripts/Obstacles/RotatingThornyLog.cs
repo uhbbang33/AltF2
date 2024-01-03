98b4dda4f29c3d7b49f4512d582188039603b8ce
MissHarvest <abcdxsky@naver.com>
2024-01-03 16:13:34 +0900

    [ Add ] Rotating Thorny Log

--- Assets/02. Scripts/Obstacles/RotatingThornyLog.cs
+++ Assets/02. Scripts/Obstacles/RotatingThornyLog.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class RotatingThornyLog : MonoBehaviour
@@ -7,9 +9,11 @@ public class RotatingThornyLog : MonoBehaviour
     public string bladePrefabName = "ThornyLog";
     [field :SerializeField] public GameObject Blade { get; private set; }
 
-    private BladeCountController[] _bladeCountControls;
+    [field :SerializeField] private BladeCountController[] _bladeCountControls;
 
     public BladeCountController.BladeCount bladeCount;
+
+    [Range(1, 3)] public float length;
 
     public float offset;
     public float angle;
@@ -21,13 +25,17 @@ public class RotatingThornyLog : MonoBehaviour
 
     private void OnValidate()
     {
-        var bladeCountControls = GetComponentsInChildren<BladeCountController>();
-        if(bladeCountControls.Length != 0 && Blade != null) 
+        var log = transform.Find("Log").gameObject;
+        log.transform.localScale = new Vector3(length, log.transform.localScale.y, log.transform.localScale.z);
+
+        if (_bladeCountControls.Length != 0 && Blade != null) 
         { 
-            foreach(var bladeCountControl in bladeCountControls)
+            foreach(var bladeCountControl in _bladeCountControls)
             {
                 bladeCountControl.count = bladeCount;
                 bladeCountControl.OnPreview(Blade, offset, angle);
+                Debug.Log($"{Math.Abs(bladeCountControl.transform.localPosition.x)} <= {length}");
+                bladeCountControl.gameObject.SetActive(Math.Abs(bladeCountControl.transform.localPosition.x) < (int)Math.Floor(length * 2)); 
             }
         }
     }
