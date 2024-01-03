64c9787ae5b445fbeb77ea2289ead2ea98e0d8cb
alstn1234 <rhalstn8549@gmail.com>
2024-01-03 20:22:00 +0900

    [Feature] Player
    - 플레이어 이동, 점프, 공격 구현
    - 카메라 구현

---
+++ Assets/02. Scripts/Player/ChickenProjectile.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ChickenProjectile : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+
+    private const float waitTime = 0.1f;
+    private const float destroyDelayTime = 3f;
+    private Rigidbody _rigidbody;
+    private Transform _mainCameraTransform;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        _mainCameraTransform = Camera.main.transform;
+        StartCoroutine(Fire());
+    }
+
+    IEnumerator Fire()
+    {
+        var dir = GetCamDir();
+        yield return new WaitForSeconds(waitTime);
+        Rotate(dir);
+        _rigidbody.velocity = dir * speed;
+        StartCoroutine(DelayDestroy(destroyDelayTime));
+    }
+
+    private Vector3 GetCamDir()
+    {
+        var fowardDir = _mainCameraTransform.forward;
+        fowardDir.Normalize();
+        return fowardDir;
+    }
+
+    private void Rotate(Vector3 dir)
+    {
+        if (dir == Vector3.zero) return;
+        Quaternion rotation = Quaternion.LookRotation(dir);
+        transform.rotation = rotation;
+    }
+
+    IEnumerator DelayDestroy(float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        Destroy(gameObject);
+    }
+}
