cf99190a28abc82f003107abf47c2e8512f80ee1
junhyeongBak <dj043045@gmail.com>
2024-01-04 21:03:59 +0900

    [Update] Elevator cs Edit
    엘리베이터 스크립트 수정

--- Assets/02. Scripts/Obstacles/Elevator.cs
+++ Assets/02. Scripts/Obstacles/Elevator.cs
@@ -54,15 +54,6 @@ public class Elevator : MonoBehaviour
 
         transform.position += movement;
 
-        // 부모-자식 관계에 있는 모든 자식 오브젝트를 이동 방향과 속도에 맞춰 이동
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            Transform child = transform.GetChild(i);
-            if (child.CompareTag("Player")) // 플레이어 태그를 가진 자식만 이동
-            {
-                child.position += (vec - this.transform.position) * Time.deltaTime;
-                vec = this.transform.position;
-            }
-        }
+        
     }
 }
